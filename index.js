67e9a5b09f8c11946d515c1391dbcec18c96cd45
xylitoli1 <henkka6henkka@gmail.com>
2025-04-21 23:41:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 const { response } = require('express');
 const { json } = require('express/lib/response');
+const path = require('path')
 var querystring = require('querystring');
 var url = require('url');
-
-
+const express = require("express")
 const app = require('express')();
 const port = process.env.PORT || 8080;
-
-let sijanti = "tyhjyys";
-
-if (typeof window !== "undefined") {
-    sijanti = window.location.href; 
-}
-
 
 let objelo;
 let objlvl;
 
-fetch('https://open.faceit.com/data/v4/players?nickname=LaterMay6e&game=cs2', {
+app.listen(port, () => {
+  console.log("toimii")
+});
+
+async function main(osote) {
+  const response = await fetch('https://open.faceit.com/data/v4/players?nickname='+osote+'&game=cs2', {
     headers: {
       'Authorization': 'Bearer 02532f35-7c82-4e0b-b436-b812544d981c',
       'Content-Type': 'application/json',
@@ -30,19 +28,17 @@ fetch('https://open.faceit.com/data/v4/players?nickname=LaterMay6e&game=cs2', {
     testi = JSON.stringify(objelo)
     testi2 = JSON.stringify(objlvl)
     console.log(data)
-   });
-
-   app.get('/elo', (req, res) => {
-    res.status(200).send({
-        faceitnickname: req.query.nimi,
-        elo: objelo,
-        lvl: objlvl,
-        testi: sijanti
-        
-    })
-})
-
-app.listen(port, () => {
-  console.log("toimii")
+   }); 
 }
-)
+
+app.get('/elo', (req, res, next) => {
+  main(req.query.nimi);
+  next();
+}, (req, res) => {
+  res.status(200).send({
+    nimi: req.query.nimi,
+    elo: objelo,
+    lvl: objlvl
+    
+});
+});
